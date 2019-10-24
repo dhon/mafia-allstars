f497a987ede285d67ad0889730eca7da270cf1c9
dhonovan <dhonovan.hauserman@sprint.com>
2019-10-24 10:21:04 -0500

    add constructor

--- src/app/services/stats.service.ts
+++ src/app/services/stats.service.ts
@@ -3,4 +3,9 @@ import * as Schemas from '../interfaces/stats.interface';
 export class StatsService {
   public mafiaGames: Schemas.MafiaGame[] = null;
   public playerStats: Schemas.PlayerStats[] = null;
+
+  constructor() {
+    this.mafiaGames = [];
+    this.playerStats = [];
+  }
 }
