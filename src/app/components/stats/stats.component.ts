d0c0b41d8bc099e18bbca43ca4287635ab7d5fcc
dhonovan <dhonovan.hauserman@sprint.com>
2019-10-25 11:44:49 -0500

    set allowSyntheticDefaultImports to true so JSONs will import correctly, and create getPlayerNames

--- src/app/components/stats/stats.component.ts
+++ src/app/components/stats/stats.component.ts
@@ -12,6 +12,7 @@ export class StatsComponent implements OnInit {
 
   ngOnInit() {
     console.log('mafiaGames: ', this.statsService.mafiaGames);
+    console.log('playerNames: ', this.statsService.playerNames);
   }
 
 }
--- src/app/services/stats.service.ts
+++ src/app/services/stats.service.ts
@@ -1,16 +1,30 @@
 import * as Schemas from '../interfaces/stats.interface';
-import * as g01 from '../../assets/2019/01.json';
-import * as g02 from '../../assets/2019/02.json';
-import * as g03 from '../../assets/2019/03.json';
-import * as g04 from '../../assets/2019/04.json';
-import * as g05 from '../../assets/2019/05.json';
+import g01 from '../../assets/2019/01.json';
+import g02 from '../../assets/2019/02.json';
+import g03 from '../../assets/2019/03.json';
+import g04 from '../../assets/2019/04.json';
+import g05 from '../../assets/2019/05.json';
 
 export class StatsService {
   public mafiaGames: Schemas.MafiaGame[] = null;
   public playerStats: Schemas.PlayerStats[] = null;
+  public playerNames: string[] = null;
 
   constructor() {
     this.mafiaGames = [g01, g02, g03, g04, g05];
+    this.playerNames = this.getPlayerNames();
     this.playerStats = [];
+  }
+
+  private getPlayerNames(): string[] {
+    const names: string[] = [];
+    this.mafiaGames.forEach(game => {
+      names.push(game.cop);
+      names.push(game.medic);
+      names.push(game.vigilante);
+      game.vanilla_town.forEach(vt => names.push(vt));
+      game.mafia.forEach(mafia => names.push(mafia));
+    });
+    return Array.from(new Set(names)); // Remove duplicates
   }
 }
