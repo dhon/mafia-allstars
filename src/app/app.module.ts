cd1b76ab87084716649ae211db56d52e565266d0
dhonovan <dhonovan.hauserman@sprint.com>
2019-10-27 07:27:42 -0500

    init header footer sidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
 import { HomeComponent } from './components/home/home.component';
 import { PlayerComponent } from './components/player/player.component';
 import { StatsComponent } from './components/stats/stats.component';
@@ -10,6 +13,9 @@ import { StatsService } from './services/stats.service';
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidenavComponent,
     HomeComponent,
     PlayerComponent,
     StatsComponent
