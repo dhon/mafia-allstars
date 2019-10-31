674fdb6d9994136238573650b34ede512039d1d4
dhonovan <dhonovan.hauserman@sprint.com>
2019-10-31 09:53:14 -0500

    move router logic to html

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,25 +7,9 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor() { }
 
   ngOnInit() {
-  }
-
-  public routeHome(): void {
-    this.router.navigate(['/']);
-  }
-
-  public routePlayers(): void {
-    this.router.navigate(['players']);
-  }
-
-  public routeStats(): void {
-    this.router.navigate(['stats']);
-  }
-
-  public routeStatsYear(year: '2017' | '2018' | '2019' | 'all'): void {
-    this.router.navigate(['stats', year]);
   }
 
 }
