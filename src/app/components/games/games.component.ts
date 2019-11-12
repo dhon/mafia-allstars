5e42131e23865c8b1bea98408d573da4b5bf421b
dhonovan <dhonovan.hauserman@sprint.com>
2019-11-12 15:45:51 -0600

    add games components logic

--- src/app/components/games/games.component.ts
+++ src/app/components/games/games.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { StatsService } from 'src/app/services/stats.service';
+import * as Schemas from 'src/app/interfaces/stats.interface';
 
 @Component({
   selector: 'app-games',
@@ -6,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./games.component.scss']
 })
 export class GamesComponent implements OnInit {
+  public games2017: Schemas.MafiaGame[];
+  public games2018: Schemas.MafiaGame[];
+  public games2019: Schemas.MafiaGame[];
 
-  constructor() { }
+  constructor(private statsService: StatsService) { }
 
   ngOnInit() {
+    this.games2017 = this.statsService.getMafiaGames('2017');
+    this.games2018 = this.statsService.getMafiaGames('2018');
+    this.games2019 = this.statsService.getMafiaGames('2019');
   }
 }
